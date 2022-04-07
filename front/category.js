f33d287ce6259bc87a8d2a086aff66100ee50ec0
이민재 <jae464@minjae-MacBookPro.local>
2022-04-08 04:18:11 +0900

    게시글등록

---
+++ front/category.js
@@ -0,0 +1,36 @@
+const first_classification = ['부문별', '공통'];
+const part_classification = [
+	'Custom 부문',
+	'Enterprise 부',
+	'AI/DX 융합사업부문',
+	'네트워크부문',
+	'IT부문',
+	'융합기술원',
+	'경영기획부문',
+	'경영지원부문',
+	'그룹 Transformation 부문',
+	'홍보실',
+	'법무실',
+	'윤리경영실',
+	'CEO 지원담당',
+	'BDO Project Group',
+	'네트워크혁신TF',
+	'안전보건총괄',
+	'(구)Cloud/IDC사업추진실',
+	'광역본부',
+];
+const area_classification = [
+	'강북/강원 광역본부',
+	'강남/서부 광역본부',
+	'부산/경남 광역본부',
+	'대구/경북 광역본부',
+	'전남/전북 광역본부',
+	'충남/충북 광역본부',
+];
+const common_classification = [
+	'회사생활',
+	'복지',
+	'상품/서비스',
+	'자회사/협력사',
+	'사내시스템',
+];
